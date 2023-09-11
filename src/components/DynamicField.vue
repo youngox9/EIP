a88dce6e4a8ea868307609f74bd69e3ac2e630ba
rex <youngox9@gmail.com>
2023-09-11 16:03:23 +0800

    add

---
+++ src/components/DynamicField.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-form-item :prop="config.fieldKey" :label="$t(config.fieldKey)">
+    <!-- 日期選擇器 -->
+    <el-date-picker
+      v-if="config.type === 'date'"
+      type="date"
+      :style="{ width: '100%' }"
+      format="YYYY-MM-DD"
+      value-format="YYYY-MM-DD"
+      placement="bottom"
+      v-bind="{ ...$attrs, ...$props }"
+    />
+    <!-- 下拉選單 -->
+    <el-select
+      v-else-if="config.type === 'select'"
+      :placeholder="$t(config.fieldKey)"
+      v-bind="{ ...$attrs, ...$props }"
+    >
+      <el-option
+        v-for="item in config?.options || []"
+        :label="item.label"
+        :value="item.value"
+      />
+    </el-select>
+    <!-- textarea -->
+    <el-input
+      v-else-if="config.type === 'textarea'"
+      autosize
+      type="textarea"
+      :placeholder="$t(config.fieldKey)"
+      v-bind="{ ...$attrs, ...$props }"
+    />
+    <el-input v-else :placeholder="$t(config.fieldKey)" />
+  </el-form-item>
+  <!-- {{ $attrs }} -->
+</template>
+
+<script setup>
+import { useStore } from "vuex";
+import { onMounted, ref, computed, reactive, watch, useAttrs } from "vue";
+
+import { useRouter } from "vue-router";
+import { useI18n } from "vue-i18n";
+import axios from "@/axios";
+import { useState, ElNotification, VALIDATIONS } from "@/utils";
+
+const attrs = useAttrs();
+const props = defineProps(["config", "modelValue"]);
+const { locale, t } = useI18n();
+const store = useStore();
+
+const ENT = computed(() => {
+  return store?.state?.global?.ENT || "";
+});
+
+const profile = computed(() => {
+  return store?.state?.global?.profile || {};
+});
+</script>
